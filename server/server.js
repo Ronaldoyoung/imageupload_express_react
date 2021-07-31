7fa97aa1fb01b06bf08804eac60de7c6710393e7
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-01 08:06:04 +0900

    이미지 저장하기 전 필터 걸어주기

--- server/server.js
+++ server/server.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const multer = require('multer');
-const { v4: uuid }  = require('uuid')
+const { v4: uuid }  = require('uuid');
 const mime = require('mime-types');
 
 console.log('uuid: ', uuid());
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "./uploads"),
-    filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`),
 });
 
-const upload = multer({ storage })
+const upload = multer({ storage, fileFilter: (req, file, cb) => { 
+    if(["image/png", "image/jpeg"].includes(file.mimetype)) cb(null, true)
+    else cb(new Error('invalid file type.'), false)
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  }
+});
 
 const app = express();
 const PORT = 5000;
 
-app.post('/upload', upload.single("image"), (req, res) => {
-    console.log(req.file)    
-    res.json(req.file);
-})
+app.use("/uploads", express.static("uploads"));
 
-app.listen(PORT, () => console.log("Express server listening on PORT " + PORT))
+
+app.post('/upload', upload.single("image"), (req, res) => {
+  console.log(req.file)    
+  res.json(req.file);
+});
+
+app.listen(PORT, () => console.log("Express server listening on PORT " + PORT));
