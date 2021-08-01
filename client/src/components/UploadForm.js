4d30101f69431235ede935390b54d136564057f8
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-01 09:24:46 +0900

    useState로 이미지 파일 데이터 관리하기

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [fileName, setFileName] = useState("이미지 파일을 업로드 해주세요.");
+
   return (
     <form>
-      <lable htmlFor="image">사진</lable>
+      <lable htmlFor="image">{fileName}</lable>
       <input 
         id="image"
         type="file"
+        onChange={e => {
+          const imageFile = e.target.files[0];
+          setFile(imageFile);
+          setFileName(imageFile.name)
+        }}
       />
       <button type="submit">제출</button>
     </form>
