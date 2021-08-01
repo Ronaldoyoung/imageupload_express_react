7a8714cbf52957719d2d4a57ed13fb18f1ced96f
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-01 10:31:00 +0900

    드래그로 이미지 올리기

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import "./UploadForm.css";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
@@ -32,13 +33,11 @@ const UploadForm = () => {
 
   return (
     <form onSubmit={onSubmit}>
-      <lable htmlFor="image">{fileName}</lable>
-      <input 
-        id="image"
-        type="file"
-        onChange={imageSelectHandler}
-      />
-      <button type="submit">제출</button>
+      <div className="file-dropper">
+        {fileName}
+        <input id="image" type="file" onChange={imageSelectHandler}/>      
+      </div>      
+      <button type="submit" style={{ width: "100%", height: 40, borderRadius: "3px", cursor: "pointer" }}>제출</button>
     </form>
   )
 }
