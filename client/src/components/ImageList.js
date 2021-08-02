1f23ea76f958c7173adf71a9f45b4284a51f6dc3
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-02 20:37:53 +0900

    useEffect로 이미지 데이터 불러오기

---
+++ client/src/components/ImageList.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ImageList = () => {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get("/images")
+    .then(result => setImages(result.data))
+    .catch((err) => console.log(err))    
+  }, [])
+
+  const imgList = images.map(image => (
+    <img style={{width: "100%"}} src={`http://localhost:5000/uploads/${image.key}`} />
+  ));
+  console.log({ images });
+
+  return (
+    <div>
+      <h3>ImageList</h3>
+      { imgList }
+    </div>
+  )
+}
+
+export default ImageList;
