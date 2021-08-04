00ea37a481627482845628ac47b678f6c49753e6
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-04 14:42:31 +0900

    회원가입 API 만들기

--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -1,16 +1,16 @@
 const { Router } = require("express");
-const ImageRouter = Router();
-const Image = require("../models/images");
+const imageRouter = Router();
+const Image = require("../models/Images");
 const { upload } = require("../middleware/imageUpload");
 
-ImageRouter.post('/images', upload.single("image"), async (req, res) => {
+imageRouter.post('/', upload.single("image"), async (req, res) => {
   const image = await new Image({ key: req.file.filename, originalFileName: req.file.originalname }).save();
   res.json(image);
 });
 
-ImageRouter.get("/images", async (req, res) => {
+imageRouter.get("/", async (req, res) => {
   const images = await Image.find();
   res.json(images);
 });
 
-module.exports = { ImageRouter };
+module.exports = { imageRouter };
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
-const { ImageRouter } = require("./routes/imageRouter");
+const { imageRouter } = require("./routes/imageRouter");
+const { userRouter } = require("./routes/userRouter");
 
 
 const app = express();
@@ -18,8 +19,9 @@ mongoose.connect(
 .then(() => {
   console.log("MongoDB Connect")
   app.use("/uploads", express.static("uploads"));
-  app.use("/images", ImageRouter)
-
+  app.use(express.json());
+  app.use("/images", imageRouter);
+  app.use("/users", userRouter);
 
   app.listen(PORT, () => console.log("Express server listening on PORT " + PORT));
 
