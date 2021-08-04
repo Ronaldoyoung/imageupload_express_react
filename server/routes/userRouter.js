d860266668ce571340cd62c82af6b131d8879fc8
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-04 16:26:33 +0900

    로그아웃 api 만들기

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,33 +2,65 @@ const { Router } = require("express");
 const userRouter = Router();
 const User = require("../models/User");
 const { hash, compare } = require("bcryptjs");
+const mongoose = require("mongoose");
+
 
 userRouter.post("/register", async (req, res) => {
   try {
     if (req.body.password.length < 6) throw new Error("low password");
     if (req.body.username.length < 3) throw new Error("username low < 3");
     const hashedPassword = await hash(req.body.password, 10);
-    await new User({
+    const user = await new User({
       name: req.body.name,
       username: req.body.username,
       hashedPassword,
+      sessions: [{createdAt: new Date()}]
     }).save();
-    res.json({ message: "user registered" });
+    const session = user.sessions[0];
+    res.json({ message: "user registered",  sessionId: session._id, name: user.name });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
 
-userRouter.post("/login", async (req, res) => {
+userRouter.patch("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
     const isValid = await compare(req.body.password, user.hashedPassword);
     if (!isValid) throw new Error("not correct information you did");
-    res.json({ message: "user logged" });
-
+    user.sessions.push({ createdAt: new Date() });
+    const session = user.sessions[user.sessions.length - 1];
+    await user.save();
+    res.json({ message: "user logged", sessionId: session._id, name: user.name });
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
 })
+
+userRouter.patch("/logout", async (req, res) => {
+  try {
+    console.log(req.headers);
+    const { sessionid } = req.headers;
+    if (!mongoose.isValidObjectId(sessionid))
+      throw new Error("invalid seesion Id");
+    const user = await User.findOne({ "sessions._id": sessionid });
+
+    if (!user) throw new Error("invalid sessionId");
+
+    await User.updateOne(
+      { _id: user.id },
+      {
+        $pull: {
+          sessions: { _id: sessionid }
+        }
+      }
+    )
+
+    res.json({ message: "user is logged out." });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 
 module.exports = { userRouter };
