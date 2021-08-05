59a38c0b2cf5303d59b451475044fe4b0ecae7e4
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-05 22:32:24 +0900

    react-router-dom 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import UploadForm from './components/UploadForm'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ImageList from './components/ImageList';
+import MainPage from "./pages/MainPage";
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from "./pages/LoginPage";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
   
   return (
-    <div style={{ maxWidth: 600, margin: "auto" }}>
-      <ToastContainer />
-      <h2>사진첩</h2>      
-      <UploadForm />
-      <ImageList />
+    <div style={{ maxWidth: 600, margin: "auto" }}>      
+      <ToastContainer />   
+      <Switch>        
+        <Route path="/auth/register" exect component={RegisterPage} />
+        <Route path="/auth/login" exect component={LoginPage} />
+        <Route path="/" component={MainPage}/>
+      </Switch>      
     </div>
   );
 }
