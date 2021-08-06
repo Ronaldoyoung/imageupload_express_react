5d0e477fe5f1f08c7b7d7436c31b9e85a69226a0
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-07 07:28:53 +0900

    권한 확인 후 이미지 저장하기

--- server/models/images.js
+++ server/models/images.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 
 const ImageSchema = new mongoose.Schema(
   {
-    key: { type: String, require: true },
-    originalFileName: { type: String, require: true},
+    user: {
+      _id: { type: mongoose.Types.ObjectId, required: true },
+      name: { type: String, required : true },
+      username: { type: String, required : true },
+    },
+    public: { type: Boolean, required: true, default: false },
+    key: { type: String, required: true },
+    originalFileName: { type: String, required: true},
   }, { timestamps: true}
 );
 
--- server/routes/imageRouter.js
+++ server/routes/imageRouter.js
@@ -5,11 +5,23 @@ const { upload } = require("../middleware/imageUpload");
 
 imageRouter.post('/', upload.single("image"), async (req, res) => {
   // 유저 정보, public 유무 확인
-  const image = await new Image({ 
-    key: req.file.filename, 
-    originalFileName: req.file.originalname 
-  }).save();
-  res.json(image);
+  try {
+    if(!req.user) throw new Error("권한이 없습니다.");
+    const image = await new Image({ 
+      user: {
+        _id: req.user.id,
+        name: req.user.name,
+        username: req.user.username
+      },
+      public: req.body.public,
+      key: req.file.filename, 
+      originalFileName: req.file.originalname 
+    }).save();
+    res.json(image);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
+  }  
 });
 
 imageRouter.get("/", async (req, res) => {
