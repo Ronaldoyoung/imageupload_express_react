6e3898a172b90a78f0f20779db7b7ef8b0a3ec78
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-06 22:58:26 +0900

    회원가입 api

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState} from "react";
 import CustomInput from "../components/CustomInput";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const RegisterPage = () => {
   const [name, setName] = useState("");  
@@ -7,15 +9,32 @@ const RegisterPage = () => {
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
 
+  const submitHandler = async e => {
+    try {
+      e.preventDefault();
+      if(username.length < 3) throw new Error("회원 ID가 너무 짧아요 3자 이상으로 해주세요.");
+      if(password.length < 6) throw new Error("비밀번호가 너무 짧아요. 6자 이상으로 해주세요.");
+      if(password !== passwordCheck) throw new Error("비밀번호가 다릅니다. 확인해 주세요.");
+
+      const result = await axios.post("/users/register", { name, username, password});
+      console.log("result : ", result);
+      toast.success("회원 가입 성공!");
+    } catch (err) {
+      toast.error(err.message);
+    }
+    
+  }
+
   return (
     <div style={{ marginTop: 100, maxWidth:350, marginLeft: "auto", marginRight: "auto"}}>
       <h3>회원가입</h3>
-      <form>        
+      <form onSubmit={submitHandler}>        
         <CustomInput label="이름" value={name} setValue={setName} />
         <CustomInput label="회원ID" value={username} setValue={setUsername} />
         <CustomInput label="비밀번호" value={password} setValue={setPassword} type="password" />
         <CustomInput label="비밀번호 확인" value={passwordCheck} setValue={setPasswordCheck} type="password" />
-      </form>
+        <button type="submit">회원가입</button>
+      </form>      
     </div>
   );
 };
