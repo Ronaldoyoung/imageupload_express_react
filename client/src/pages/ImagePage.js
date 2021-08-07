0f797925587df77065d166535913d459cc3dc2a8
ronaldoyoung <ronaldoyoung@gmail.com>
2021-08-07 16:57:01 +0900

    좋아요 적용하기

--- client/src/pages/ImagePage.js
+++ client/src/pages/ImagePage.js
@@ -1,12 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useParams } from 'react-router'
 import { ImageContext } from '../context/ImageContext';
+import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
 
 const ImagePage = () => {
   const { imageId } = useParams();
-  const { images, myImages } = useContext(ImageContext);
+  const { images, myImages, setImages, setMyImages } = useContext(ImageContext);
   const image = images.find(image => image._id === imageId ) 
     || myImages.find(image => image._id === imageId);
+  const [hasLiked, setHasLiked] = useState(false);
+  const [me] = useContext(AuthContext);
+
+  useEffect(() => {
+    if(me && image && image.likes.includes(me.userId)) {
+      setHasLiked(true);
+    }
+  },[me, image]);
+
+  const updateImage = (images, image) => [
+    ...images.filter(image => image._id !== imageId), 
+    image
+  ].sort((a,b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime());
+  
+
+  const onSubmit = async () => {
+    const result = await axios.patch(`/images/${imageId}/${hasLiked? "unlike" : "like"}`)
+    if(result.data.public) 
+      setImages(updateImage(images, result.data));
+    else 
+      setMyImages(updateImage(myImages, result.data));
+    setHasLiked(!hasLiked);    
+  }
 
   if(!image) return <h2>Loading...</h2>
 
@@ -16,7 +41,11 @@ const ImagePage = () => {
       <img
         style={{ width: "100%"}} 
         alt={imageId} src={`http://localhost:5000/uploads/${image.key}`}
-      />      
+      />
+      <span>좋아요 {image.likes.length}</span>
+      <button style={{float:"right"}} onClick={onSubmit}>
+        {hasLiked ? "좋아요 취소" : "좋아요"}
+      </button>
     </div>
   );
 }
